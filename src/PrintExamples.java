08c54044f626a59f50746544c71bd0b23a2ce5d5
Cody Stevens <stevca9@Codys-MBP.dhcp.wfu.edu>
2023-08-29 19:44:47 -0400

    Initial commit

---
+++ src/PrintExamples.java
@@ -0,0 +1,42 @@
+public class PrintExamples {
+
+    // This is the main method, and the beginning of the program
+    public static void main( String[] args ) {
+
+        // This is a single comment, when a line is commented it's invisible to the program
+
+        /*
+           This is a block comment
+           It comments multiple lines
+           across several rows
+        */
+
+        // helloWorld(); // This line calls the method helloWorld
+
+        /*
+        printVariables(); // This line calls the method printVariables
+        */
+    }
+
+    public static void helloWorld( ) {
+
+        System.out.print("Hello"); // print the String "Hello"
+        System.out.print("World"); // print the String "World"
+
+        // How could you add a space between Hello and World?
+        // How could you print Hello and World on different lines?
+    }
+
+    public static void printVariables( ) {
+
+        int x; // This line declares a variable x. What type of value can be stored in x?
+
+        // x = // Update this line to store an appropriate value in x.
+
+        // Once a variable has been declared and assigned a value it can then be referenced later in the program.
+
+        // System.out.println(x);
+        // System.out.println("The value of x is" + x );
+
+    }
+}
